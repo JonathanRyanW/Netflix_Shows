b394e26a958415312e57753dd796c18e84efd017
Jonathan Ryan <jonathanryan576@gmail.com>
2021-02-25 05:20:10 +0700

    Initial Commit

---
+++ Unsolved Problems.R
@@ -0,0 +1,9 @@
+"""Belum tau caranya:
+  Mau cari tau proporsi movie dan tvshow dari film yang ditambahkan untuk
+  setiap tahunnya. Siapa tau ada trend dimana jenis film movie makin lama
+  semakin berkurang atau jenis tvshow makin lama makin bertambah.
+  
+  Pengen cari tau juga apakah semakin kesini ada negara yang filmnya semakin sering
+  ditambahkan ke netflix. Untuk mengetahui hal ini kolom country harus bisa lebih
+  di sederhanakan.
+"""
